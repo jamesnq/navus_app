a04d5c465b1ab5d9814eeedf03f5ae072d0af2e2
Huy <dodaihoc318@gmail.com>
2021-11-22 11:13:51 +0700

    sua file

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:navus_app/vehicle_assignment_screen.dart';
+import 'package:navus_app/widget/rounded_button.dart';
+import 'package:navus_app/widget/text_field.dart';
 import 'constain.dart';
-
-bool isHiddenPassword = true;
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -19,176 +19,65 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       backgroundColor: backgroundColor,
       body: SingleChildScrollView(
-          child: Container(
-              //padding: const EdgeInsets.only(left:32, right: 32),
-
-              height: size.height,
-              width: size.width,
-              child: Center(
-                child: Column(
-                  //mainAxisAlignment: MainAxisAlignment.spaceAround,
-
-                  //alignment: Alignment.center,
-                  children: [
-                    Spacer(
-                      flex: 6,
-                    ),
-                    //logo
-
-                    SvgPicture.asset(assets_logo, alignment: Alignment.center),
-
-                    Spacer(
-                      flex: 1,
-                    ),
-
-                    //text sign in to your account
-                    const Text(
-                      "Sign in to your account",
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: textColor,
-                      ),
-                    ),
-
-                    Spacer(
-                      flex: 1,
-                    ),
-
-                    //input data
-                    //ID
-                    InputData(
-                        textHint: "Fleet ID", icon: assets_id, size: size),
-
-                    Spacer(),
-
-                    //name
-                    InputData(
-                        textHint: "Username",
-                        icon: assets_userName,
-                        size: size),
-
-                    Spacer(
-                      flex: 1,
-                    ),
-
-//passwor       //password
-                    InputData(
-                        textHint: "Password",
-                        icon: assets_lock,
-                        size: size,
-                        password: true,
-                        onTap: () {
-                          setState(() {
-                            isHiddenPassword = !isHiddenPassword;
-                          });
-                        }),
-
-                    Spacer(
-                      flex: 1,
-                    ),
-
-                    //forgot password
-                    Container(
-                        width: MediaQuery.of(context).size.width * 0.9,
-                        child: Align(
-                          alignment: Alignment.bottomRight,
-                          child: Text(
-                            "Forgot Password?",
-                            style: TextStyle(color: Color(0xFF747D96)),
-                          ),
-                        )),
-
-                    Spacer(
-                      flex: 5,
-                    ),
-
-                    //login button
-                    roundedButton(
-                        text: "Login",
-                        onPressed: () {
-                          var route = MaterialPageRoute(
-                              builder: (context) => VehiCleAssignmentScreen());
-                          Navigator.push(context, route);
-                        }),
-
-                    Spacer(
-                      flex: 3,
-                    ),
-                  ],
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Center(
+            child: Column(
+              children: [
+                const Spacer(flex: 6),
+                SvgPicture.asset(assets_logo, alignment: Alignment.center),
+                const Spacer(flex: 1),
+                const Text(
+                  "Sign in to your account",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: textColor,
+                  ),
                 ),
-              ))),
-    );
-  }
-}
-
-Widget InputData(
-    {required String? textHint,
-    required String icon,
-    required size,
-    bool password = false,
-    onTap}) {
-  if (password == false) {
-    return Container(
-      width: size!.width * 0.9,
-      child: TextFormField(
-          decoration: InputDecoration(
-        border: UnderlineInputBorder(
-          borderSide: BorderSide(style: BorderStyle.solid, color: textColor),
-        ),
-        prefixIcon: SizedBox(
-          child: SvgPicture.asset(
-            icon,
-            fit: BoxFit.scaleDown,
+                const Spacer(flex: 1),
+                const TextFieldCustom(
+                  placeholder: "Fleet ID",
+                  leafIcon: assets_id,
+                ),
+                const Spacer(),
+                const TextFieldCustom(
+                  placeholder: "Username",
+                  leafIcon: assets_userName,
+                ),
+                const Spacer(flex: 1),
+                const TextFieldCustom(
+                  placeholder: "Password",
+                  leafIcon: assets_lock,
+                  isPassword: true,
+                ),
+                const Spacer(flex: 1),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: const Align(
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(color: Color(0xFF747D96)),
+                    ),
+                  ),
+                ),
+                const Spacer(flex: 5),
+                RoundedButton(
+                  text: "Login",
+                  onPressed: () {
+                    var route = MaterialPageRoute(
+                        builder: (context) => VehiCleAssignmentScreen());
+                    Navigator.push(context, route);
+                  },
+                ),
+                const Spacer(flex: 3),
+              ],
+            ),
           ),
         ),
-        hintText: textHint,
-        hintStyle: TextStyle(color: textColor, fontSize: 14),
-      )),
-    );
-  } else {
-    return Container(
-      width: size!.width * 0.9,
-      child: TextFormField(
-          obscureText: isHiddenPassword,
-          decoration: InputDecoration(
-            border: UnderlineInputBorder(
-              borderSide:
-                  BorderSide(style: BorderStyle.solid, color: textColor),
-            ),
-            hintText: textHint,
-            hintStyle: TextStyle(color: textColor, fontSize: 14),
-            prefixIcon: SizedBox(
-              child: SvgPicture.asset(
-                icon,
-                fit: BoxFit.scaleDown,
-              ),
-            ),
-            suffixIcon: InkWell(
-                // alignment: Alignment.center,
-                onTap: onTap,
-                child: Icon(
-                  Icons.remove_red_eye_outlined,
-                  color: textColor,
-                )),
-          )),
+      ),
     );
   }
 }
 
-Widget roundedButton({required String text, required onPressed}) {
-  return Container(
-      child: SizedBox(
-    width: 311,
-    height: 56,
-    child: TextButton(
-      onPressed: onPressed,
-      child: Text(text, style: TextStyle(fontSize: 16)),
-      style: ButtonStyle(
-        foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-        backgroundColor: MaterialStateProperty.all<Color>(buttonColor),
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.5))),
-      ),
-    ),
-  ));
-}
--- lib/vehicle_assignment_screen.dart
+++ lib/vehicle_assignment_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:navus_app/widget/information_box.dart';
+import 'package:navus_app/widget/rounded_button.dart';
 import 'login_screen.dart';
 import 'constain.dart';
 
@@ -41,16 +43,18 @@ class _VehiCleAssignmentScreenState extends State<VehiCleAssignmentScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Spacer(),
-                informationBox(text: "Vehicle", rightText: "Add a Vehicle"),
+                InformationBox(text: "Vehicle", rightText: "Add a Vehicle"),
                 Spacer(),
-                informationBox(text: "Trailers", rightText: "Add a Trailer"),
+                InformationBox(text: "Trailers", rightText: "Add a Trailer"),
                 Spacer(),
-                informationBox(
+                InformationBox(
                     text: "Shipping IDs",
                     rightText: "Add a Shipping ID",
-                    id: "197687"),
+                    id: "197687",
+                    isId: true,
+                ),
                 Spacer(flex: 8),
-                roundedButton(text: "Done", onPressed: () {}),
+                RoundedButton(text: "Done", onPressed: () {}),
                 Spacer(flex: 9),
               ],
             ),
@@ -61,92 +65,92 @@ class _VehiCleAssignmentScreenState extends State<VehiCleAssignmentScreen> {
   }
 }
 
-Widget informationBox(
-    {required String text, required String rightText, String? id}) {
-  if (id == null) {
-    return Container(
-      color: Colors.white,
-      padding: EdgeInsets.only(left: 16, right: 16),
-      height: 100,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Center(
-            child: Text(
-              text,
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 16,
-                color: appBarTextColor,
-              ),
-            ),
-          ),
-          Spacer(),
-          Center(
-            child: Text(
-              rightText,
-              textAlign: TextAlign.right,
-              style: TextStyle(
-                fontSize: 16,
-                color: activeTextColor,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  } else {
-    return Container(
-      color: Colors.white,
-      padding: EdgeInsets.only(left: 16, right: 16),
-      height: 100,
-      child: Column(
-        children: [
-          Spacer(flex: 2),
-          //Row 1
-          Row(
-            children: [
-              Center(
-                child: Expanded(
-                  child: Text(
-                    text,
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: appBarTextColor,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Spacer(),
-          //Row 2
-          Row(
-            children: [
-              Center(
-                child: Text(
-                  id,
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-              Spacer(),
-              Center(
-                child: Text(
-                  rightText,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: activeTextColor,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Spacer(flex: 2),
-        ],
-      ),
-    );
-  }
-}
+// Widget informationBox(
+//     {required String text, required String rightText, String? id}) {
+//   if (id == null) {
+//     return Container(
+//       color: Colors.white,
+//       padding: EdgeInsets.only(left: 16, right: 16),
+//       height: 100,
+//       child: Row(
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: [
+//           Center(
+//             child: Text(
+//               text,
+//               textAlign: TextAlign.left,
+//               style: TextStyle(
+//                 fontSize: 16,
+//                 color: appBarTextColor,
+//               ),
+//             ),
+//           ),
+//           Spacer(),
+//           Center(
+//             child: Text(
+//               rightText,
+//               textAlign: TextAlign.right,
+//               style: TextStyle(
+//                 fontSize: 16,
+//                 color: activeTextColor,
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   } else {
+//     return Container(
+//       color: Colors.white,
+//       padding: EdgeInsets.only(left: 16, right: 16),
+//       height: 100,
+//       child: Column(
+//         children: [
+//           Spacer(flex: 2),
+//           //Row 1
+//           Row(
+//             children: [
+//               Center(
+//                 child: Expanded(
+//                   child: Text(
+//                     text,
+//                     textAlign: TextAlign.left,
+//                     style: TextStyle(
+//                       fontSize: 16,
+//                       color: appBarTextColor,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//           Spacer(),
+//           //Row 2
+//           Row(
+//             children: [
+//               Center(
+//                 child: Text(
+//                   id,
+//                   style: TextStyle(
+//                     fontSize: 16,
+//                   ),
+//                 ),
+//               ),
+//               Spacer(),
+//               Center(
+//                 child: Text(
+//                   rightText,
+//                   style: TextStyle(
+//                     fontSize: 16,
+//                     color: activeTextColor,
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//           Spacer(flex: 2),
+//         ],
+//       ),
+//     );
+//   }
+// }
